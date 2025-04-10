f6a496838901881e5b87f09fbd107e02b041c0fa
hmuhammad.c <hmuhammad.c@tahakom.com>
2025-04-10 08:04:46 +0300

    fixed account creation

--- src/main/java/com/manpower/service/SponsorService.java
+++ src/main/java/com/manpower/service/SponsorService.java
@@ -2,6 +2,7 @@ package com.manpower.service;
 
 import com.manpower.model.Sponsor;
 import com.manpower.repository.SponsorRepository;
+import com.manpower.util.SecurityUtil;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -9,9 +10,11 @@ import java.util.Optional;
 @Service
 public class SponsorService {
     private final SponsorRepository sponsorRepository;
+    private final CompanyService companyService;
 
-    public SponsorService(SponsorRepository sponsorRepository) {
+    public SponsorService(SponsorRepository sponsorRepository, CompanyService companyService) {
         this.sponsorRepository = sponsorRepository;
+        this.companyService = companyService;
     }
 
     public List<Sponsor> getAllSponsors() {
@@ -22,7 +25,8 @@ public class SponsorService {
         return sponsorRepository.findById(id);
     }
 
-    public Sponsor createSponsor(Sponsor sponsor) {
+    public Sponsor createSponsor(Sponsor sponsor) { //TODO: MAKE DTO
+        sponsor.setCompany(companyService.getCompanyById(SecurityUtil.getCompanyClaim()).get());
         return sponsorRepository.save(sponsor);
     }
 
