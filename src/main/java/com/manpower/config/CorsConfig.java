af8b2a8f1d145246c8e27bea104ec568e9894a03
hmuhammad.c <hmuhammad.c@tahakom.com>
2025-04-21 12:34:42 +0300

    updated CORS

--- src/main/java/com/manpower/config/CorsConfig.java
+++ src/main/java/com/manpower/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("https://manpower-fe-production.up.railway.app")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
