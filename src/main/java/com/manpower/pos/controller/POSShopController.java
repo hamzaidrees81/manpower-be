2c9b3d42280c74ea1c89c83ff076406518b224a8
hmuhammad.c <hmuhammad.c@tahakom.com>
2025-05-27 05:26:32 +0300

    updated URL

--- src/main/java/com/manpower/pos/controller/POSShopController.java
+++ src/main/java/com/manpower/pos/controller/POSShopController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/shops")
+@RequestMapping("/pos/api/shops")
 @RequiredArgsConstructor
 public class POSShopController {
 
